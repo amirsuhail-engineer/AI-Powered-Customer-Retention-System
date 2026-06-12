7907195619677b3a75cf56593c96d27c9573695f
Amir Suhail <amirsuhailengg01@gmail.com>
2026-06-12 12:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,5 +131,6 @@ def predict():
         return render_template('index.html', prediction='Error occurred. Please check inputs.')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
